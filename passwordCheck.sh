406c5ed466fa2705a79324395631c97c232c66ed
dparlitu <DPARLITU@our.ecu.edu.au>
2021-08-16 09:16:08 +0800

    remove txt extension from password file

--- passwordCheck.sh
+++ passwordCheck.sh
@@ -9,28 +9,28 @@ if [ -f secret ]; then
     read -sp "$(echo -e "\033[34mPlease enter the Password:\e[0m ")" pwd
 
     # save password to file
-    echo $pwd > password.txt
+    echo $pwd > password
 else
 
     # ask user for a new password password
     read -sp "$(echo -e "\e[33mPlease choose a NEW Password:\e[0m ")" pwd
 
     # save password to file
-    echo $pwd > password.txt
+    echo $pwd > password
 
     # create hash for password file
-    sha256sum password.txt > secret
+    sha256sum password > secret
 fi
 
 # clean up a bit the feedback window with an empty line
 echo
 
 # if the result from checking the hash is a failure, show deny message and exit with 1 otherwise show message with access granted and exit with 0
-if [ "$(sha256sum -c secret)" == "password.txt: FAILED" ]; then
+if [ "$(sha256sum -c secret)" == "password: FAILED" ]; then
     echo -e "\033[31mAccess Denied!\e[0m"
     echo
     # clear stored password for security purposes
-    echo "" > password.txt
+    echo "" > password
     exit 1
 else
     echo
@@ -38,16 +38,6 @@ else
     echo
 
     # clear stored password for security purposes
-    echo "" > password.txt
+    echo "" > password
     exit 0
 fi
-
-
-# References
-# https://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
-#   used for if file exists operators
-
-# https://gist.github.com/onnimonni/b49779ebc96216771a6be3de46449fa1
-#   used the script to learn how to check if a file already exists for my IF statement
-
-# https://phoenixnap.com/kb/md5sum-linux
